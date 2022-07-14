72e846c7785447ddb628804bda914a5459f1605b
Andrew Faben <andrew.faben@os.uk>
2022-07-14 13:58:11 +0100

    Moved app files into app folder and created separate test files for each class

---
+++ tests/test_book_normaliser.py
@@ -0,0 +1,28 @@
+import pytest
+from pathlib import Path
+from html_getter_parser import get_book_parser, normalise_book, get_boilerplate_indices,\
+remove_boilerplate, remove_numbers_symbols, lower_strip_clean, sort_alphabetical, get_parsed_text
+
+def test_get_book_parser():
+    pass
+
+def test_normalise_book():
+    pass
+
+def test_get_boilerplate_indices():
+    pass
+
+def test_remove_boilerplate():
+    pass
+
+def test_remove_numbers_symbols():
+    pass
+
+def test_lower_strip_clean():
+    pass
+
+def test_sort_alphabetical():
+    pass
+
+def test_get_parsed_text():
+    pass
